15a7af2359646d06086f1848c8b3e1607ba90b74
Thilina Kumarasiri <gihanthilina1997@gmail.com>
2025-06-19 17:35:50 +0530

    start development of portfolio backend

--- src/App.js
+++ src/App.js
@@ -7,11 +7,25 @@ import { CertificatesSection } from './components/CertificateSection';
 import { ContactSection } from './components/ContactSection';
 import { AboutMeSection } from './components/AboutMeSection';
 import { FooterSection } from './components/FooterSection';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect (() => {
+    axios.get('http://localhost:8000/api/projects/')
+    .then(response => {
+      setData(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}, []);
+
   return (
+
     <div className="App">
       <NavBar />
       <MainBanner />
@@ -22,6 +36,11 @@ function App() {
       <AboutMeSection />
       <ContactSection />
       <FooterSection />
+      <div>
+      {data.map(item => (
+        <div key={item.id}>{item.name}</div>
+      ))}
+      </div>
     </div>
   );
 }
